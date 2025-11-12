ce3f6b5c4b9bf118b3d5ef2390e5436b64eefc03
Kocksxdoze <famelezzz@gmail.com>
2025-11-12 21:57:15 +0500

    'api'

--- utils/api.js
+++ utils/api.js
@@ -1,14 +1,9 @@
 export function getApiBaseUrl() {
-  // Если определен переменная окружения, используем её
-  if (process.env.NEXT_PUBLIC_API_URL) {
-    return process.env.NEXT_PUBLIC_API_URL;
+  if (typeof window !== "undefined") {
+    const host = window.location.hostname;
+    console.log(host);
+    return `http://${host}:4000`;
   }
 
-  // Для продакшена
-  if (process.env.NODE_ENV === "production") {
-    return "https://your-production-domain.com:4000";
-  }
-
-  // Для разработки
   return "http://localhost:4000";
 }
